15486b460bdf1cf136265679c6623bca43c16997
Irina Berendeeva <55141843+irinaBerendeeva87@users.noreply.github.com>
2024-10-06 10:07:21 +0200

    Add CRUD Interface, CheckedBase and UncheckedBase classes

--- src/main/java/com/example/teamcity/api/requests/Request.java
+++ src/main/java/com/example/teamcity/api/requests/Request.java
@@ -7,8 +7,8 @@ public class Request  {
     /**
      * Request it's a class describing request parameters such as: specification, endpoint(relative URL, model)
      */
-    private final RequestSpecification spec;
-    private final Endpoint endpoint;
+    protected final RequestSpecification spec;
+    protected final Endpoint endpoint;
 
     public Request(RequestSpecification spec, Endpoint endpoint) {
         this.spec = spec;
