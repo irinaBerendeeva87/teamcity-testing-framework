814dfe9682ef29b5cfb0a297a58c49e561973c99
Irina Berendeeva <55141843+irinaBerendeeva87@users.noreply.github.com>
2024-10-08 21:16:58 +0200

    add pattern Facade

--- src/test/java/com/example/teamcity/api/BuildTypeTest.java
+++ src/test/java/com/example/teamcity/api/BuildTypeTest.java
@@ -1,45 +1,42 @@
 package com.example.teamcity.api;
 
-import com.example.teamcity.api.enums.Endpoint;
 import com.example.teamcity.api.models.BuildType;
 import com.example.teamcity.api.models.Project;
 import com.example.teamcity.api.models.User;
-import com.example.teamcity.api.requests.checked.CheckedBase;
+import com.example.teamcity.api.requests.checked.CheckedRequests;
 import com.example.teamcity.api.spec.Specifications;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 
+import static com.example.teamcity.api.enums.Endpoint.*;
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 import static io.qameta.allure.Allure.step;
+
 @Test(groups = {"Regressions"})
-public class BuildTypeTest extends BaseApiTest{
+public class BuildTypeTest extends BaseApiTest {
     @Test(description = "User should be able to create build type", groups = {"Positive", "CRUD"})
     public void userCreatesBuildTypeTest() {
         var user = generate(User.class);
 
-        var userRequester = new CheckedBase<User>(Specifications.superUserSpec(), Endpoint.USERS);
-        userRequester.create(user);
+        superUserCheckRequests.getRequest(USERS).create(user);
+        var userCheckRequests = new CheckedRequests(Specifications.authSpec(user));
 
         var project = generate(Project.class);
 
-        var projectRequester = new CheckedBase<Project>(Specifications.authSpec(user),Endpoint.PROJECTS);
-        project = projectRequester.create(project);
+        project = userCheckRequests.<Project>getRequest(PROJECTS).create(project);
 
         var buildType = generate(Arrays.asList(project), BuildType.class);
 
-        var buildTypRequester = new CheckedBase<BuildType>(Specifications.authSpec(user),Endpoint.BUILD_TYPES);
+        userCheckRequests.getRequest(BUILD_TYPES).create(buildType);
 
-        buildTypRequester.create(buildType) ;
+        var createdBuildType = userCheckRequests.<BuildType>getRequest(BUILD_TYPES).read(buildType.getId());
 
-        var createdBuildtype=  buildTypRequester.read(buildType.getId() );
-
-        softy.assertEquals(buildType.getName(), createdBuildtype.getName(), "Build type name is not correct ");
-
+        softy.assertEquals(buildType.getName(), createdBuildType.getName(), "Build type name is not correct");
     }
 
-    @Test(description = "User should not be able to create two build type with the same id", groups = {"Negative","CRUD"})
-    public void userCreatesTwoBuildTypesWithTheSameIdTest () {
+    @Test(description = "User should not be able to create two build type with the same id", groups = {"Negative", "CRUD"})
+    public void userCreatesTwoBuildTypesWithTheSameIdTest() {
         step("Create user");
         step("Create project by user");
         step("Create buildType1 for project by user");
@@ -47,8 +44,8 @@ public class BuildTypeTest extends BaseApiTest{
         step("Check buildType2 was not created with bad request code");
     }
 
-    @Test(description = "Project admin should be able to create build type for their project", groups = {"Positive","Roles "})
-    public void projectsAdminCreatesBuildTypeTest () {
+    @Test(description = "Project admin should be able to create build type for their project", groups = {"Positive", "Roles "})
+    public void projectsAdminCreatesBuildTypeTest() {
         step("Create user");
         step("Create project");
         step("Grant user PROJECT_ADMIN role in  project");
@@ -57,8 +54,8 @@ public class BuildTypeTest extends BaseApiTest{
         step("Check buildType was created successfully ");
     }
 
-    @Test(description = "Project admin should not be able to create build type for another user project", groups = {"Negative","Roles "})
-    public void projectsAdminCreatesBuildTypeForAnotherUserProjectTest  () {
+    @Test(description = "Project admin should not be able to create build type for another user project", groups = {"Negative", "Roles "})
+    public void projectsAdminCreatesBuildTypeForAnotherUserProjectTest() {
         step("Create user1");
         step("Create project1");
         step("Grant user1 PROJECT_ADMIN role in  project1");
